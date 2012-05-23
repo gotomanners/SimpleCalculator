b48eaa391b683e64728f1d966500ffe5df34849a
Manners Oshafi <manners@gotomanners.com>
2012-05-23 18:34:03 +0100

    Extrapolated calculator logic to a seperate class
    Improved calculator structure and robustness

--- src/com/gotomanners/calculator/Calculator.java
+++ src/com/gotomanners/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package com.gotomanners.calculator;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: moshafi
@@ -8,4 +14,112 @@ package com.gotomanners.calculator;
  * To change this template use File | Settings | File Templates.
  */
 public class Calculator {
+
+    private static final String LOG_TAG = "SimpleCalculator";
+    private static Context context = null;
+    double total;
+    ArrayList<String> numbersEntered;
+    ArrayList<String> operationsEntered;
+
+    public Calculator() {
+        this.total = 0.0;
+        numbersEntered = new ArrayList<String>();
+        operationsEntered = new ArrayList<String>();
+    }
+
+    public static void setContext(Context context) {
+        Calculator.context = context;
+    }
+
+    protected double solve() throws Exception {
+        if(numbersEntered != null && operationsEntered != null) {
+            if(numbersEntered.size() > 0 && operationsEntered.size() > 0) {
+                if((numbersEntered.size() - 1) == operationsEntered.size()) {
+                    String number;
+                    String operation = "";
+                    for (int i=0; i<numbersEntered.size(); i++) {
+                        number = numbersEntered.get(i);
+                        if(i ==0){
+                            setTotal(stringToDouble(number));
+                        } else {
+                            operation = operationsEntered.get(i-1);
+                            if(operation.equals("+")) {
+                                this.add(number);
+                            }
+                            if(operation.equals("-")) {
+                                this.subtract(number);
+                            }
+                            if(operation.equals("*")) {
+                                this.multiply(number);
+                            }
+                            if(operation.equals("/")) {
+                                if(number.equals("0")) {
+                                    clearAll();
+                                    throw new CannotDivideByZeroException(context.getString(R.string.error_divide_by_zero));
+//                                    return 0.0;
+                                }
+                                this.divide(number);
+                            }
+                        }
+                    }
+                } else {
+                    System.out.println("numbers and operations size do not match!");
+                    return 0.0;
+                }
+            }
+        }
+        return this.total;
+    }
+
+    private double add(String num) {
+
+        return this.total += stringToDouble(num);
+    }
+
+    private double subtract(String num) {
+
+        return this.total -= stringToDouble(num);
+    }
+
+    private double multiply(String num) {
+
+        return this.total *= stringToDouble(num);
+    }
+
+    private double divide(String num) {
+
+        return this.total /= stringToDouble(num);
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    protected void clearAll() {
+        setTotal(0.0);
+        numbersEntered.clear();
+        operationsEntered.clear();
+    }
+
+    private double stringToDouble(String number) {
+        return Double.parseDouble(number);
+    }
+
+    private String doubleToString(double number) {
+        return String.valueOf(number);
+    }
+}
+
+class CannotDivideByZeroException extends Exception {
+    CannotDivideByZeroException() {
+        super();
+    }
+
+    CannotDivideByZeroException(String detailMessage) {
+        super(detailMessage);
+    }
 }
--- src/com/gotomanners/calculator/SimpleCalculatorActivity.java
+++ src/com/gotomanners/calculator/SimpleCalculatorActivity.java
@@ -10,179 +10,216 @@ import android.widget.Toast;
 
 public class SimpleCalculatorActivity extends Activity implements View.OnClickListener {
 
-private static final String LOG_TAG = "SimpleCalculator";
-private EditText calcOutput;
-private Button button0;
-private Button button1;
-private Button button2;
-private Button button3;
-private Button button4;
-private Button button5;
-private Button button6;
-private Button button7;
-private Button button8;
-private Button button9;
-private Button button_clear;
-private Button button_decimal_point;
+    private static final String LOG_TAG = "SimpleCalculator";
+    private EditText calcOutput;
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button_clear;
+    private Button button_decimal_point;
 
-private Button button_operator_divide;
-private Button button_operator_multiply;
-private Button button_operator_subtract;
-private Button button_operator_add;
+    private Button button_operator_divide;
+    private Button button_operator_multiply;
+    private Button button_operator_subtract;
+    private Button button_operator_add;
 
-private Button button_operator_equals;
+    private Button button_operator_equals;
 
-private String lastNumber;
-private String lastOperation;
+    private String lastNumber;
+    private boolean looseOperatorPresent;
+    private boolean shouldResetAfterResult;
+
+    private Calculator calculator;
 
 
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-/**
- * Called when the activity is first created.
- */
-@Override
-public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.main);
+        calcOutput = (EditText) this.findViewById(R.id.calcOutput);
 
-	calcOutput = (EditText) this.findViewById(R.id.calcOutput);
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button_clear = (Button) findViewById(R.id.button_clear);
+        button_decimal_point = (Button) findViewById(R.id.button_decimal_point);
 
-	button0 = (Button) findViewById(R.id.button0);
-	button1 = (Button) findViewById(R.id.button1);
-	button2 = (Button) findViewById(R.id.button2);
-	button3 = (Button) findViewById(R.id.button3);
-	button4 = (Button) findViewById(R.id.button4);
-	button5 = (Button) findViewById(R.id.button5);
-	button6 = (Button) findViewById(R.id.button6);
-	button7 = (Button) findViewById(R.id.button7);
-	button8 = (Button) findViewById(R.id.button8);
-	button9 = (Button) findViewById(R.id.button9);
-	button_clear = (Button) findViewById(R.id.button_clear);
-	button_decimal_point = (Button) findViewById(R.id.button_decimal_point);
+        button_operator_divide = (Button) findViewById(R.id.button_operator_divide);
+        button_operator_multiply = (Button) findViewById(R.id.button_operator_multiply);
+        button_operator_subtract = (Button) findViewById(R.id.button_operator_subtract);
+        button_operator_add = (Button) findViewById(R.id.button_operator_add);
+        button_operator_equals = (Button) findViewById(R.id.button_operator_equals);
 
-	button_operator_divide = (Button) findViewById(R.id.button_operator_divide);
-	button_operator_multiply = (Button) findViewById(R.id.button_operator_multiply);
-	button_operator_subtract = (Button) findViewById(R.id.button_operator_subtract);
-	button_operator_add = (Button) findViewById(R.id.button_operator_add);
-	button_operator_equals = (Button) findViewById(R.id.button_operator_equals);
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button_clear.setOnClickListener(this);
+        button_decimal_point.setOnClickListener(this);
 
-	button0.setOnClickListener(this);
-	button1.setOnClickListener(this);
-	button2.setOnClickListener(this);
-	button3.setOnClickListener(this);
-	button4.setOnClickListener(this);
-	button5.setOnClickListener(this);
-	button6.setOnClickListener(this);
-	button7.setOnClickListener(this);
-	button8.setOnClickListener(this);
-	button9.setOnClickListener(this);
-	button_clear.setOnClickListener(this);
-	button_decimal_point.setOnClickListener(this);
+        button_operator_divide.setOnClickListener(this);
+        button_operator_multiply.setOnClickListener(this);
+        button_operator_subtract.setOnClickListener(this);
+        button_operator_add.setOnClickListener(this);
+        button_operator_equals.setOnClickListener(this);
 
-	button_operator_divide.setOnClickListener(this);
-	button_operator_multiply.setOnClickListener(this);
-	button_operator_subtract.setOnClickListener(this);
-	button_operator_add.setOnClickListener(this);
-	button_operator_equals.setOnClickListener(this);
+        calculator = new Calculator();
+        Calculator.setContext(this);
+        lastNumber = "";
 
-}
+    }
 
-private void appendNumberPressedToCalcOutput(View view) {
-	if(calcOutput.getText().toString().equals("0")) {
-		calcOutput.setText(((Button) view).getText());
-	} else {
-		calcOutput.append(((Button)view).getText());
-	}
-}
-
-private void clearOutputText(View view) {
-	calcOutput.setText("0");
-    this.lastNumber = "";
-    this.lastOperation = "";
-}
-
-private void recordNumberPressed(View view) {
-	this.lastNumber = ((Button) view).getText().toString();
-}
-
-private void recordOperationPressed(final View view) {
-	this.lastNumber = calcOutput.getText().toString();
-	this.lastOperation = ((Button) view).getText().toString();
-
-	clearOutputText(view);
-//    calcOutput.setText(this.lastNumber+this.lastOperation);
-}
-
-private String performCalcOperation(View view, String operation) {
-	if(operation != null) {
-		try{
-            double valueOfPreviousTextInOutput = Double.parseDouble(this.lastNumber);
-            double valueOfCurrentTextInOutput = Double.parseDouble(calcOutput.getText().toString());
-            double result = 0;
-
-            if(operation.equals("/")) {
-                if(valueOfCurrentTextInOutput == 0.0) {
-                    Toast.makeText(view.getContext(),getString(R.string.error_divide_b_zero), Toast.LENGTH_SHORT).show();
-                    return getString(R.string.error_error_string);
-                }
-                result = (valueOfPreviousTextInOutput / valueOfCurrentTextInOutput);
-            }
-            if(operation.equals("*")) {
-                result = (valueOfPreviousTextInOutput * valueOfCurrentTextInOutput);
-            }
-            if(operation.equals("-")) {
-                result = (valueOfPreviousTextInOutput - valueOfCurrentTextInOutput);
-            }
-            if(operation.equals("+")) {
-                result = (valueOfPreviousTextInOutput + valueOfCurrentTextInOutput);
-            }
-
-            String textResult = String.valueOf(result);
-            if(textResult.endsWith(".0")) {
-                textResult = String.valueOf(Math.round(result));
-            }
-
-            return textResult;
-        } catch (Exception e) {
-            Toast.makeText(view.getContext(),e.getMessage(), Toast.LENGTH_LONG).show();
-            Log.v(LOG_TAG, e.getMessage());
+    private void appendNumberPressedToCalcOutput(View view) {
+        if(shouldResetAfterResult) {
+            resetCalculator();
+            shouldResetAfterResult = false;
         }
-	}
-    return getString(R.string.default_return_string);
-}
+        String numPressed = ((Button) view).getText().toString();
+        if (calcOutput.getText().toString().equals("0")) {
+            calcOutput.setText(numPressed);
+        } else {
+            calcOutput.append(numPressed);
+        }
 
+        lastNumber += numPressed;
+        looseOperatorPresent = false;
 
-@Override
-public void onClick(View view) {
-	switch (view.getId()) {
-		case R.id.button0:
-		case R.id.button1:
-		case R.id.button2:
-		case R.id.button3:
-		case R.id.button4:
-		case R.id.button5:
-		case R.id.button6:
-		case R.id.button7:
-		case R.id.button8:
-		case R.id.button9:
-		case R.id.button_decimal_point:
-			appendNumberPressedToCalcOutput(view);
-			break;
-		case R.id.button_operator_divide:
-		case R.id.button_operator_multiply:
-		case R.id.button_operator_subtract:
-		case R.id.button_operator_add:
-			recordOperationPressed(view);
-			break;
-		case R.id.button_clear: {
-			clearOutputText(view);
-			break;
-		}
-		case R.id.button_operator_equals: {
-			String result = performCalcOperation(view, this.lastOperation);
-            calcOutput.setText(result);
-			break;
-		}
-	}
-}
+        Log.i(LOG_TAG, "appended= "+lastNumber);
+    }
+
+    private void appendOperationPressedToCalcOutput(View view) {
+        if(!looseOperatorPresent) {
+            Log.i(LOG_TAG, "No immediate existing operator");
+            String operation = ((Button) view).getText().toString();
+            calcOutput.append(operation);
+            recordOperationPressed(view);
+            looseOperatorPresent = true;
+            shouldResetAfterResult = false;
+
+            Log.i(LOG_TAG, "Appending "+((Button) view).getText());
+        }
+    }
+
+    private void resetCalculator() {
+        calcOutput.setText("0");
+        lastNumber = "";
+        calculator.clearAll();
+
+        Log.i(LOG_TAG, "cleared output");
+    }
+
+    private void recordNumberEntered() {
+        calculator.numbersEntered.add(this.lastNumber);
+        Log.i(LOG_TAG, "last number recorded= "+lastNumber);
+
+        lastNumber = "";
+        looseOperatorPresent = false;
+    }
+
+    private void recordOperationPressed(View view) {
+        String op = ((Button) view).getText().toString();
+
+        recordNumberEntered();
+        calculator.operationsEntered.add(op);
+
+        Log.i(LOG_TAG, "last operation recorded= " + op);
+    }
+
+    private void displayResult() {
+        String result = removeUnnecessaryDecimalZero(calculator.getTotal());
+        calcOutput.setText(result);
+
+        Log.i(LOG_TAG, "Displaying Result " + result);
+    }
+
+    private void performCalculation(View view) {
+        Log.i(LOG_TAG, "Performing Calculation");
+
+        try {
+            recordNumberEntered();
+
+            double result = calculator.solve();
+            this.lastNumber =removeUnnecessaryDecimalZero(result);
+
+            displayResult();
+
+            calculator.clearAll();
+            shouldResetAfterResult = true;
+
+        } catch (CannotDivideByZeroException cdz) {
+            Toast.makeText(this, getString(R.string.error_divide_by_zero), Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, cdz.getMessage());
+            resetCalculator();
+        } catch (Exception e) {
+            Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e(LOG_TAG, e.getMessage());
+            resetCalculator();
+        }
+        looseOperatorPresent = false;
+    }
+
+    private String removeUnnecessaryDecimalZero(double number) {
+        String textResult = String.valueOf(number);
+        if (textResult.endsWith(".0")) {
+            textResult = String.valueOf(Math.round(number));
+        }
+        return textResult;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button0:
+            case R.id.button1:
+            case R.id.button2:
+            case R.id.button3:
+            case R.id.button4:
+            case R.id.button5:
+            case R.id.button6:
+            case R.id.button7:
+            case R.id.button8:
+            case R.id.button9:
+            case R.id.button_decimal_point:
+                appendNumberPressedToCalcOutput(view);
+                break;
+            case R.id.button_operator_divide:
+            case R.id.button_operator_multiply:
+            case R.id.button_operator_subtract:
+            case R.id.button_operator_add:
+                appendOperationPressedToCalcOutput(view);
+                break;
+            case R.id.button_clear: {
+                resetCalculator();
+                break;
+            }
+            case R.id.button_operator_equals: {
+                performCalculation(view);
+                break;
+            }
+        }
+    }
 }
