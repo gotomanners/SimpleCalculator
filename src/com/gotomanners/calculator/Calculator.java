7e9172f8f9cb18b59d0b1de69bd6a54eb6175ffc
Manners Oshafi <manners@gotomanners.com>
2012-05-23 17:40:15 +0100

    fixed typo in string resource

---
+++ src/com/gotomanners/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.gotomanners.calculator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: moshafi
+ * Date: 5/23/12
+ * Time: 2:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+}
